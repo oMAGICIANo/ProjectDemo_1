831ae63238a1f385c511bcb3e2ae815ee70159a4
MAGICIAN <MAGICIAN@MSI>
2022-02-19 12:33:46 +0800

    加入專案檔案。

---
+++ ProjectDemo_1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace ProjectDemo_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMousePositionX = new System.Windows.Forms.Label();
+            this.labelMousePositionY = new System.Windows.Forms.Label();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.labelState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelMousePositionX
+            // 
+            this.labelMousePositionX.AutoSize = true;
+            this.labelMousePositionX.BackColor = System.Drawing.Color.White;
+            this.labelMousePositionX.Location = new System.Drawing.Point(12, 9);
+            this.labelMousePositionX.Name = "labelMousePositionX";
+            this.labelMousePositionX.Size = new System.Drawing.Size(127, 15);
+            this.labelMousePositionX.TabIndex = 1;
+            this.labelMousePositionX.Text = "labelMousePositionX";
+            // 
+            // labelMousePositionY
+            // 
+            this.labelMousePositionY.AutoSize = true;
+            this.labelMousePositionY.Location = new System.Drawing.Point(12, 35);
+            this.labelMousePositionY.Name = "labelMousePositionY";
+            this.labelMousePositionY.Size = new System.Drawing.Size(126, 15);
+            this.labelMousePositionY.TabIndex = 1;
+            this.labelMousePositionY.Text = "labelMousePositionY";
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(597, 12);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(75, 23);
+            this.buttonRestart.TabIndex = 2;
+            this.buttonRestart.Text = "重新生成";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.Location = new System.Drawing.Point(145, 9);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(64, 15);
+            this.labelState.TabIndex = 3;
+            this.labelState.Text = "labelState";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.labelMousePositionY);
+            this.Controls.Add(this.labelMousePositionX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Project Demo 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelMousePositionX;
+        private System.Windows.Forms.Label labelMousePositionY;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Label labelState;
+    }
+}
+
---
+++ ProjectDemo_1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjectDemo_1.Properties;
+
+namespace ProjectDemo_1
+{
+    public partial class Form1 : Form
+    {
+
+        private const int row = 6, colume = 5;
+        private PictureBox[,] pictureBoxeGrid = new PictureBox[row, colume];
+        private Label[,] labelGridNumber = new Label[row, colume];
+        private Panel panelGrid;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGrid();
+        }
+
+        private void InitializeGrid() 
+        {
+            this.panelGrid = new Panel();
+            this.panelGrid.Name = "panelGrid";
+            this.panelGrid.Size = new Size(660, 550);
+            this.panelGrid.Location = new Point(15, 100);
+
+
+            for (int i = 0; i < row; i++) 
+            {
+                for (int j = 0; j < colume; j++) 
+                {
+                    this.pictureBoxeGrid[i, j] = new PictureBox();
+                    this.pictureBoxeGrid[i, j].Location = new Point(0 + i * 110, 0 + j * 110);
+                    this.pictureBoxeGrid[i, j].Size = new Size(100, 100);
+                    this.pictureBoxeGrid[i, j].SizeMode = PictureBoxSizeMode.Zoom;
+                    this.pictureBoxeGrid[i, j].Name = "pictureBoxeGrid" + i.ToString() + j.ToString();
+
+                    RandomBead(this.pictureBoxeGrid[i, j]);
+
+                    this.pictureBoxeGrid[i, j].MouseUp += new MouseEventHandler(this.pictureBoxeGrid_MouseUp);
+                    this.pictureBoxeGrid[i, j].MouseDown += new MouseEventHandler(this.pictureBoxeGrid_MouseDown);
+                    this.pictureBoxeGrid[i, j].MouseMove += new MouseEventHandler(this.pictureBoxeGrid_MouseMove);
+
+                    this.labelGridNumber[i, j] = new Label();
+                    this.labelGridNumber[i, j].Name = "labelGridNumber" + i.ToString() + j.ToString();
+                    this.labelGridNumber[i, j].Text = i.ToString() + j.ToString();
+                    this.labelGridNumber[i, j].Location = new Point(40 + i * 110, 40 + j * 110);
+                    this.labelGridNumber[i, j].Size = new Size(30, 30);
+
+                    this.panelGrid.Controls.Add(this.labelGridNumber[i, j]);
+                    this.panelGrid.Controls.Add(this.pictureBoxeGrid[i, j]);
+
+                    this.Controls.Add(panelGrid);
+                    
+                }
+            }
+        }
+
+        private void RandomBead(PictureBox p) 
+        {
+            Random myRandom = new Random();
+
+            int imageIndex = myRandom.Next(0, 5);
+
+            switch (imageIndex)
+            {
+                case 0:
+                    p.Image = Resources.red_bead;
+                    break;
+                case 1:
+                    p.Image = Resources.orange_bead;
+                    break;
+                case 2:
+                    p.Image = Resources.green_bead;
+                    break;
+                case 3:
+                    p.Image = Resources.blue_bead;
+                    break;
+                case 4:
+                    p.Image = Resources.purple_bead;
+                    break;
+            }
+
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            this.panelGrid.Dispose();
+
+            InitializeGrid();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //this.labelMousePositionX.Text = "X:" + e.Location.X.ToString();
+            //this.labelMousePositionY.Text = "Y:" + e.Location.Y.ToString();
+        }
+
+        bool moveFlag = false;
+        int startX, startY;
+
+        private void pictureBoxeGrid_MouseDown(object sender, MouseEventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+
+            moveFlag = true;
+
+            startX = p.Location.X;
+            startY = p.Location.Y;
+        }
+
+        private void pictureBoxeGrid_MouseUp(object sender, MouseEventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+
+            moveFlag = false;
+
+            p.Location = new Point(startX, startY);
+        }
+
+        private void pictureBoxeGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+
+            this.labelState.Text = p.Name;
+
+            if (moveFlag)
+            {
+                Int32 LocationX = Form1.MousePosition.X, LocationY = Form1.MousePosition.Y;
+                this.labelMousePositionX.Text = "X:" + LocationX.ToString();
+                this.labelMousePositionY.Text = "Y:" + LocationY.ToString();
+
+                p.Location = new Point(LocationX - this.Location.X - this.panelGrid.Location.X - 60,
+                                       LocationY - this.Location.Y - this.panelGrid.Location.Y - 60);
+            }
+        }
+
+
+    }
+}
